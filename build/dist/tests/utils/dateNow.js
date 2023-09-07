d20788eaa6a7c7c4d2264fb03131c5d8d05e670a
Abishri765 <beingshriabi@gmail.com>
2023-09-07 09:23:32 +0530

    first commit

--- build/dist/tests/utils/dateNow.js
+++
@@ -1,7 +0,0 @@
-export async function dateNow(){
-    const timeApi = "https://worldtimeapi.org/api/timezone/Asia/Kolkata";
-    const response = await fetch(timeApi, { method: "GET" });
-    const responseData = await response.json();
-    const unixtime = responseData.unixtime
-    return unixtime;
-};
